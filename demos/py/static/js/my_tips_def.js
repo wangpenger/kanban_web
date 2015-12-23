7c4269ef91f45d2a4651edbe22ed1d7bbcbed407
wangpeng <wangpeng-0210@163.com>
2015-12-23 09:12:12 +0800

    fix two issues for card arrangement show and burndown page
    1. improve the arrangement for the card in home page.
    2. improve the person select in burn down window.

--- demos/py/static/js/my_tips_def.js
+++ demos/py/static/js/my_tips_def.js
@@ -1,9 +1,10 @@
 (function($){
 	$.fn.wish = function() {
 		var _this = this;
+        //get the body area height(backlog/ongoing/pending/done weight is a little less than this value).
+        main_area_height = $("body").height();
 		var _wish = _this.children();
 		var _wish_len = _wish.length;
-		
 		var wish = {
 			area:{
 				left:0,
@@ -50,25 +51,27 @@
 			var _p = methods.pos();
 			var _s = methods.css();
 			var _self = $(this);
-			var orderID = _self.val();
             
-            _left_pos = 0;
-            if(orderID % 2 == 0)
-                _left_pos = 10;
-            else if(orderID % 3 == 0)
-                _left_pos = 20;
-            else
-                _left_pos = 1;
-            _left_pos = Math.random() * _left_pos;
+            //remove old algorithm for the random height of each card, but it's not very good-looking for large-scale screen( just so so for laptop).
+			var orderID = _self.val();
+
+            _left_pos = Math.random();
             
             task_key_id = _self.attr("id");
             task_key_id = task_key_id.split('_')[0];
 
-            orderID = orderID - first_element_value + 1;
-			orderID = (orderID==10 ? 9 : orderID % 10) * 35 - 30;
-            orderID = orderID<=0 ? -orderID : orderID
+            //new algorithm for the random height of each card.
+            orderID_new = _self.height() * 0.3 + (i * (_self.height() - 10));
+            while(orderID_new >= main_area_height*0.8 - _self.height())
+            {
+                j = (orderID_new - main_area_height*0.8) / _self.height();
+                orderID_new = 60 + (j * (_self.height()));
+                vertical_area_width = _this.parent().parent().width();
+                max_pos = vertical_area_width * 0.5 > wish.area.right ? wish.area.right : 45;
+                _left_pos = max_pos + (15 * Math.random());
+            }
 			_self.prepend('<a href="javascript:delete_task('+task_key_id+');" class="wish-close"></a>');
-			_self.addClass('wish').addClass('s'+_s).css({'position':'absolute', 'left':_left_pos + 'px', 'top':orderID + 'px'});
+			_self.addClass('wish').addClass('s'+_s).css({'position':'absolute', 'left':_left_pos + 'px', 'top':orderID_new + 'px'});
 			_self.hover(function(){
 				_self.css({'z-index':'9999','border':'none'}).children('.wish-close').show().bind('click',function(){_self.effect('scale',{percent: 0},200,function(){_self.remove()})});
 			},function(){
